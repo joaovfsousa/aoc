7cce3f78e1a3f151ef377f7b0f947671c53e03b8
João Vitor Sousa <joaovfsousa@gmail.com>
2023-12-07 00:38:06 -0300

    feat(2023): day 2 both parts

---
+++ src/years/2023/days/day-02.ts
@@ -0,0 +1,86 @@
+import { Solution } from '@src/solution';
+
+interface GameSet {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+type Game = GameSet[];
+
+class DaySolution extends Solution {
+  private games: Game[];
+
+  parsePart1(): void {
+    this.games = this.lines.map((line) =>
+      line
+        .split(': ') // game: sets
+        .at(1)!
+        .split('; ') // set1; set 2; set 3
+        .map((set) => {
+          return set
+            .split(', ') // 1 color1, 2 color2, 3 color3
+            .reduce(
+              (acc, countAndColor) => {
+                const [count, color] = countAndColor.split(' '); // 1 color1 => ['1', 'color1']
+                return { ...acc, [color]: parseInt(count) };
+              },
+              { red: 0, green: 0, blue: 0 }
+            );
+        })
+    );
+  }
+
+  part1(): string {
+    const maxCubesPerSet: GameSet = {
+      red: 12,
+      green: 13,
+      blue: 14,
+    };
+
+    return this.games
+      .reduce((acc, game, index) => {
+        const isPossible = game.every(
+          (set) =>
+            set.red <= maxCubesPerSet.red &&
+            set.green <= maxCubesPerSet.green &&
+            set.blue <= maxCubesPerSet.blue
+        );
+
+        if (isPossible) {
+          return acc + index + 1;
+        }
+
+        return acc;
+      }, 0)
+      .toString();
+  }
+
+  parsePart2(): void {
+    void 'Not to do';
+  }
+
+  part2(): string {
+    return this.games
+      .map((game) => {
+        return game.reduce(
+          (acc, set) => {
+            (Object.keys(set) as (keyof GameSet)[]).forEach((color) => {
+              if (set[color] > acc[color]) {
+                acc[color] = set[color];
+              }
+            });
+
+            return acc;
+          },
+          { red: 0, green: 0, blue: 0 }
+        );
+      })
+      .reduce((acc, set) => {
+        return acc + set.red * set.green * set.blue;
+      }, 0)
+      .toString();
+  }
+}
+
+export const solution = new DaySolution();
