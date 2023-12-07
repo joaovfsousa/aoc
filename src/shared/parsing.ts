d591f0f612dba43f17556e7c8510402b7ac60aa7
João Vitor Sousa <joaovfsousa@gmail.com>
2023-12-07 13:24:50 -0300

    feat(2023): day 3 part 1

---
+++ src/shared/parsing.ts
@@ -0,0 +1,3 @@
+export function isDigit(digit: string): boolean {
+  return digit >= '0' && digit <= '9';
+}
---
+++ src/years/2023/days/day-03.ts
@@ -0,0 +1,97 @@
+import { isDigit } from '@src/shared/parsing';
+import { Solution } from '@src/solution';
+
+interface Position {
+  x: number;
+  y: number;
+}
+
+class SSymbol {
+  constructor(
+    public value: string,
+    public position: Position
+  ) {}
+}
+
+class SNumber {
+  constructor(
+    public value: string,
+    public position: Position,
+    public lastDigitX: number
+  ) {}
+
+  toNumber(): number {
+    return parseInt(this.value);
+  }
+
+  isAdjacentToASymbol(symbols: SSymbol[]): boolean {
+    return symbols.some((symbol) => {
+      if (Math.abs(symbol.position.y - this.position.y) > 1) return false;
+
+      return (
+        symbol.position.x >= this.position.x - 1 &&
+        symbol.position.x <= this.lastDigitX + 1
+      );
+    });
+  }
+}
+
+class DaySolution extends Solution {
+  private numbers: SNumber[] = [];
+  private symbols: SSymbol[] = [];
+
+  parsePart1() {
+    let actualNumber: SNumber | undefined;
+
+    this.lines.forEach((line, y) => {
+      line.split('').forEach((char, x) => {
+        if (isDigit(char)) {
+          if (actualNumber) {
+            actualNumber.value += char;
+            actualNumber.lastDigitX = x;
+          } else {
+            actualNumber = new SNumber(char, { x, y }, x);
+          }
+          return;
+        }
+
+        if (actualNumber) {
+          this.numbers.push(actualNumber);
+          actualNumber = undefined;
+        }
+
+        if (char === '.') return;
+
+        this.symbols.push(new SSymbol(char, { x, y }));
+      });
+    });
+
+    return 'DONE';
+  }
+
+  part1(): string {
+    return this.numbers
+      .filter((num) => {
+        const a = num.isAdjacentToASymbol(this.symbols);
+        return a;
+      })
+      .reduce((acc, cur) => {
+        return acc + cur.toNumber();
+      }, 0)
+      .toString();
+  }
+
+  parsePart2() {
+    this.lines.map((line) => {
+      void line;
+    });
+
+    return 'TODO';
+  }
+
+  part2(): string {
+    return 'TODO';
+  }
+}
+
+export const solution = new DaySolution();
