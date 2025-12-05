b535cbad93412ccce8a1cc026bdfd2b0bde59749
João Vitor Sousa <joaovfsousa@gmail.com>
2025-12-04 23:42:24 -0300

    solution 2025 day04 part2

--- solutions/2025/day04.go
+++ solutions/2025/day04.go
@@ -42,8 +42,8 @@ func (d Day4) Part1(inputPath string) (any, error) {
 			}
 
 			for _, d := range adjacentDirs {
-				x := c + d[0]
-				y := r + d[1]
+				x := c + d[1]
+				y := r + d[0]
 
 				if x >= 0 && x < colCount && y >= 0 && y < rowCount {
 					if string(rows[y][x]) == "@" {
@@ -62,10 +62,82 @@ func (d Day4) Part1(inputPath string) (any, error) {
 	return totalAccessible, nil
 }
 
+func replaceInStringMatrix(m []string, i [2]int) []string {
+	rm := []string{}
+
+	r, c := i[0], i[1]
+
+	rowToReplace := m[r]
+
+	prevRows := m[:r]
+	nextRows := m[r+1:]
+
+	prevCols := rowToReplace[:c]
+	nextCols := rowToReplace[c+1:]
+
+	rowAfterReplace := prevCols + "x" + nextCols
+
+	rm = append(rm, prevRows...)
+	rm = append(rm, rowAfterReplace)
+	rm = append(rm, nextRows...)
+
+	return rm
+}
+
 func (d Day4) Part2(inputPath string) (any, error) {
-	// TODO: implement
-	// Use one of the readers from pkg/aoc/readers.go
-	return nil, nil
+	rows := []string{}
+	totalRemoved := 0
+
+	for l := range aoc.IterLines(inputPath) {
+		rows = append(rows, l)
+	}
+
+	rowCount := len(rows)
+	colCount := len(rows[0])
+
+	var removedCount int
+
+	for ok := true; ok; ok = (removedCount != 0) {
+		toRemove := [][2]int{}
+
+		for r := range rowCount {
+			for c := range colCount {
+				totalRolls := 0
+
+				if string(rows[r][c]) != "@" {
+					continue
+				}
+
+				for _, d := range adjacentDirs {
+					x := c + d[1]
+					y := r + d[0]
+
+					if x >= 0 && x < colCount && y >= 0 && y < rowCount {
+						if string(rows[y][x]) == "@" {
+							totalRolls++
+						}
+					}
+				}
+
+				if totalRolls < 4 {
+					log.Debugf("x=%v, y=%v", c, r)
+					toRemove = append(toRemove, [2]int{r, c})
+				}
+			}
+		}
+
+		removedCount = len(toRemove)
+
+		log.Debugf("Removed %v rolls", removedCount)
+
+		totalRemoved += removedCount
+
+		for _, tr := range toRemove {
+			rows = replaceInStringMatrix(rows, tr)
+		}
+	}
+
+	return totalRemoved, nil
 }
 
 func init() {
