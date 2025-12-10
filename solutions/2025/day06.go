db380f12a3b94d3af85bdd74a518ab5d143d418d
João Vitor Sousa <joaovfsousa@gmail.com>
2025-12-09 23:38:09 -0300

    solution 2025 day06 part2

--- solutions/2025/day06.go
+++ solutions/2025/day06.go
@@ -70,10 +70,71 @@ func (d Day6) Part1(inputPath string) (any, error) {
 	return total, nil
 }
 
+// TODO: refactor to loop only once
 func (d Day6) Part2(inputPath string) (any, error) {
-	// TODO: implement
-	// Use one of the readers from pkg/aoc/readers.go
-	return nil, nil
+	lines := aoc.ReadLines(inputPath)
+
+	input := []string{}
+
+	isFirstColumn := true
+
+	for c := 0; c < len(lines[0]); c++ {
+		cvb := strings.Builder{}
+
+		for _, line := range lines {
+			cvb.WriteByte(line[c])
+		}
+
+		colValue := strings.TrimSpace(cvb.String())
+
+		if len(colValue) == 0 {
+			isFirstColumn = true
+		} else {
+			if isFirstColumn {
+				isFirstColumn = false
+
+				op := colValue[len(colValue)-1]
+				value := strings.TrimSpace(colValue[:len(colValue)-1])
+
+				input = append(input, string(op))
+				input = append(input, value)
+			} else {
+				input = append(input, colValue)
+			}
+		}
+	}
+
+	total := 0
+
+	accumulated := 0
+	var op string
+
+	for _, item := range input {
+		if item == "*" {
+			total += accumulated
+			accumulated = 1
+			op = item
+			continue
+		}
+
+		if item == "+" {
+			total += accumulated
+			accumulated = 0
+			op = item
+			continue
+		}
+
+		if op == "*" {
+			accumulated *= str.StringToInt(item)
+			continue
+		}
+
+		accumulated += str.StringToInt(item)
+	}
+
+	total += accumulated
+
+	return total, nil
 }
 
 func init() {
